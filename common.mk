edb0a6526dcfbdeb67ecd4b30637d7665beabc01
Anay Wadhera <awadhera@berkeley.edu>
2021-10-14 21:49:33 -0700

    sm8350-common: build 2.3 fingerprint HIDL
    Change-Id: I1ec10ccd2207c5ed4cd8cbd072ec0558b9117494

--- common.mk
+++ common.mk
@@ -306,8 +306,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
